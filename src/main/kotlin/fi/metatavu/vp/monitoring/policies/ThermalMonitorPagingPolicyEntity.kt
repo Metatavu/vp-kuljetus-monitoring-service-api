c77d483e31522973c1ea3da4f3e980a12c6c7749
Simeon <simeon.platonov@gmail.com>
2025-03-11 11:51:02 +0200

    Paging policy entity

---
+++ src/main/kotlin/fi/metatavu/vp/monitoring/policies/ThermalMonitorPagingPolicyEntity.kt
@@ -0,0 +1,35 @@
+package fi.metatavu.vp.monitoring.policies
+
+import fi.metatavu.vp.api.model.PagingPolicyContact
+import fi.metatavu.vp.monitoring.persistence.Metadata
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.Id
+import jakarta.persistence.ManyToOne
+import jakarta.persistence.Table
+import jakarta.validation.constraints.NotEmpty
+import java.util.*
+
+@Entity
+@Table(name = "thermalmonitorpagingpolicy")
+class ThermalMonitorPagingPolicyEntity: Metadata() {
+    @Id
+    lateinit var id: UUID
+
+    @Column
+    @NotEmpty
+    lateinit var policyType: String
+
+    @Column(nullable = false)
+    var priority: Int? = null
+
+    @Column(nullable = false)
+    var escalationSeconds: Int? = null
+
+    @ManyToOne
+    lateinit var pagingPolicyContact: PagingPolicyContact
+
+    override lateinit var creatorId: UUID
+
+    override lateinit var lastModifierId: UUID
+}

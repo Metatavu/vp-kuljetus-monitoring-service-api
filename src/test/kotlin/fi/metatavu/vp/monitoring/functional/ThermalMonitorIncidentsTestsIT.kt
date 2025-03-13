9c326d7df42efd8862710484945e52690ed36dcd
Simeon <simeon.platonov@gmail.com>
2025-03-13 19:23:53 +0200

    Testing low threshold incidents

--- src/test/kotlin/fi/metatavu/vp/monitoring/functional/ThermalMonitorIncidentsTestsIT.kt
+++ src/test/kotlin/fi/metatavu/vp/monitoring/functional/ThermalMonitorIncidentsTestsIT.kt
@@ -10,6 +10,7 @@ import fi.metatavu.vp.test.client.models.ThermalMonitorStatus
 import io.quarkus.test.junit.QuarkusTest
 import io.quarkus.test.junit.TestProfile
 import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertNotNull
 import org.junit.jupiter.api.Test
 import java.time.OffsetDateTime
 import java.util.*
@@ -24,12 +25,13 @@ class ThermalMonitorIncidentsTestsIT: AbstractFunctionalTest() {
     @Test
     fun testCreateThermalMonitorIncident() = createTestBuilder().use {
         val thermometerId = UUID.randomUUID()
+        val thermometer2Id = UUID.randomUUID()
 
         val monitor = it.manager.thermalMonitors.create(
             ThermalMonitor(
                 name = "Monitor",
                 status = ThermalMonitorStatus.ACTIVE,
-                thermometerIds = arrayOf(thermometerId),
+                thermometerIds = arrayOf(thermometerId, thermometer2Id),
                 lowerThresholdTemperature = -50f,
                 upperThresholdTemperature = 50f
             )
@@ -80,5 +82,21 @@ class ThermalMonitorIncidentsTestsIT: AbstractFunctionalTest() {
         assertEquals(thermometerId, incident.thermometerId, "Incident thermometerId should be $thermometerId")
         assertEquals(120f, incident.temperature, "Incident temperature should be 120f")
         assertEquals(ThermalMonitorIncidentStatus.TRIGGERED, incident.status, "Incident status should be TRIGGERED")
+
+        MessagingClient.publishMessage(
+            TemperatureGlobalEvent(
+                thermometerId = thermometer2Id,
+                temperature = -60f,
+                timestamp = OffsetDateTime.now().toInstant().toEpochMilli()
+            ),
+            routingKey = RoutingKey.TEMPERATURE
+        )
+
+        Thread.sleep(1000)
+        val incidents2 = it.manager.incidents.listThermalMonitorIncidents()
+        assertEquals(2, incidents2.size, "There should be exactly two incidents")
+        val incident2 = incidents2.find { listIncident -> listIncident.thermometerId == thermometer2Id }
+        assertNotNull(incident2)
+        assertEquals(-60f, incident2!!.temperature, "Incident temperature should be -60f")
     }
 }
